c2e05b1e7dea9c9726a83d77f5c080cd4749759f
Peter Leung <ys.leung.uk@gmail.com>
2019-01-20 19:14:19 +0800

    Added a new class ApiHandler to start fetching data.

---
+++ RevolutCurrencies/ApiHandler.swift
@@ -0,0 +1,24 @@
+//
+//  ApiHandler.swift
+//  RevolutCurrencies
+//
+//  Created by Peter Leung on 20/1/2019.
+//  Copyright © 2019 Peter Leung. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class ApiHandler {
+    
+    func requestData(_ requestURL:String, completionHandler: @escaping(Data) -> Void) {
+        let manager = Alamofire.SessionManager.default
+        manager.session.configuration.timeoutIntervalForRequest = 120
+        
+        manager.request(requestURL, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: [:]).responseJSON { (response) in
+            
+            guard let data = response.data else { return }
+            completionHandler(data)
+        }
+    }
+}
