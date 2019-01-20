9d232f171457f52f842a62ea3a16b26dc4d548df
Peter Leung <ys.leung.uk@gmail.com>
2019-01-20 18:53:27 +0800

    Added the code to put the table view in the main interface.

--- RevolutCurrencies/MainVC.swift
+++ RevolutCurrencies/MainVC.swift
@@ -9,11 +9,28 @@
 import UIKit
 
 class MainVC: UIViewController {
+    
+    //Adding table view and the cell name
+    let cellName = "cell"
+    
+    let ratesTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Trying to add the table view to the main interface
+        view.addSubview(ratesTableView)
+        setupLayout()
     }
 
+    //MARK: Adding the auto layout constraints for the table view
+    func setupLayout() {
+        let constraints = [ratesTableView.topAnchor.constraint(equalTo: view.topAnchor), ratesTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor), ratesTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),  ratesTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)]
+        NSLayoutConstraint.activate(constraints)
+    }
 }
 
