4542f54d30e2d22a376935cced202611aa0cace9
Peter Leung <ys.leung.uk@gmail.com>
2019-01-22 10:53:55 +0800

    Added some basic Unit Test code.

--- RevolutCurrenciesTests/RevolutCurrenciesTests.swift
+++ RevolutCurrenciesTests/RevolutCurrenciesTests.swift
@@ -10,24 +10,27 @@ import XCTest
 @testable import RevolutCurrencies
 
 class RevolutCurrenciesTests: XCTestCase {
+    
+    var mainVC: MainVC!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        mainVC = MainVC()
+        mainVC.loadViewIfNeeded()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        mainVC = nil
+        super.tearDown()
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
+    func testPerformance() {
         self.measure {
-            // Put the code you want to measure the time of here.
+            mainVC.apiHandler.requestData("https://revolut.duckdns.org/latest?base=\(mainVC.currentBaseCurrency)", completionHandler: { (any) in
+            })
         }
     }
 
