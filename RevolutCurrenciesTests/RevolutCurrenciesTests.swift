0be6570273b0dfb44a43b7dfaae2890e76f4535e
Peter Leung <ys.leung.uk@gmail.com>
2019-01-22 11:27:09 +0800

    Modified the unit test due to the new data handler.

--- RevolutCurrenciesTests/RevolutCurrenciesTests.swift
+++ RevolutCurrenciesTests/RevolutCurrenciesTests.swift
@@ -13,40 +13,36 @@ class RevolutCurrenciesTests: XCTestCase {
     
     var mainVC: MainVC!
     var promise: XCTestExpectation!
-
+    
     override func setUp() {
         mainVC = MainVC()
         mainVC.loadViewIfNeeded()
         promise = expectation(description: "Test Passed!")
     }
-
+    
     override func tearDown() {
         mainVC = nil
         super.tearDown()
     }
-
+    
     //Testing async progress
     func testDownloadData() {
-        mainVC.apiHandler.requestData("https://revolut.duckdns.org/latest?base=\(mainVC.currentBaseCurrency)", completionHandler: { (returnedData) in
-            
-            if let ratesDictionary = returnedData as? [String : Any] {
-                if let rates = ratesDictionary["rates"] as? [String : Any] {
-                    if rates.count > 0 {
-                        self.promise.fulfill()
-                    } else {
-                        XCTFail("Returned data is nil")
-                    }
-                }
+        mainVC.dataHandler.getData(mainVC.currentBaseCurrency, completionHandler: { (rates) in
+            if rates.count > 0 {
+                self.promise.fulfill()
+            } else {
+                XCTFail("Returned data is nil")
             }
         })
         waitForExpectations(timeout: 3, handler: nil)
     }
-
+    
     func testPerformance() {
         self.measure {
-            mainVC.apiHandler.requestData("https://revolut.duckdns.org/latest?base=\(mainVC.currentBaseCurrency)", completionHandler: { (any) in
+            mainVC.dataHandler.getData(mainVC.currentBaseCurrency, completionHandler: { (rates) in
+                
             })
         }
     }
-
+    
 }
